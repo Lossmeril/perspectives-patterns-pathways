4672ff5b0eb4e87f452ad2978b94e190d8127968
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2025-02-16 19:47:05 +0000

    style: socials buttons icons

--- components/socialsButtons.tsx
+++ components/socialsButtons.tsx
@@ -1,21 +1,22 @@
 import Link from "next/link";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const SocialsButtons = () => {
   return (
     <div className="hidden md:flex flex-col gap-5 fixed left-0 top-1/2 -translate-y-3/4 z-[100]">
       <Link href="" target="_blank">
         <div className="grid place-content-center w-14 h-14 bg-black hover:bg-red transition-all text-white">
-          FB
+          <FaFacebook size={24} />
         </div>
       </Link>
       <Link href="" target="_blank">
         <div className="grid place-content-center w-14 h-14 bg-black hover:bg-red transition-all text-white">
-          IG
+          <FaInstagram size={26} />
         </div>
       </Link>
       <Link href="" target="_blank">
         <div className="grid place-content-center w-14 h-14 bg-black hover:bg-red transition-all text-white">
-          LI
+          <FaLinkedin size={26} />
         </div>
       </Link>
     </div>
