af3cba0d55184a199552720f751c79a6cfb30de5
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2025-02-17 10:49:23 +0000

    fix: socials buttons now have global links in them

--- components/socialsButtons.tsx
+++ components/socialsButtons.tsx
@@ -1,20 +1,21 @@
+import { facebook, instagram, linkedin } from "@/datasets/links";
 import Link from "next/link";
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const SocialsButtons = () => {
   return (
     <div className="hidden md:flex flex-col gap-5 fixed left-0 top-1/2 -translate-y-3/4 z-[100]">
-      <Link href="" target="_blank">
+      <Link href={facebook} target="_blank">
         <div className="grid place-content-center w-14 h-14 bg-black hover:bg-red transition-all text-beige">
           <FaFacebook size={24} />
         </div>
       </Link>
-      <Link href="" target="_blank">
+      <Link href={instagram} target="_blank">
         <div className="grid place-content-center w-14 h-14 bg-black hover:bg-red transition-all text-beige">
           <FaInstagram size={26} />
         </div>
       </Link>
-      <Link href="" target="_blank">
+      <Link href={linkedin} target="_blank">
         <div className="grid place-content-center w-14 h-14 bg-black hover:bg-red transition-all text-beige">
           <FaLinkedin size={26} />
         </div>
