6c3c268e4ba05c7aab09a73a8abaaf31b8bb70b3
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2025-02-19 14:24:49 +0000

    fix: broken linkedin link

--- datasets/links.tsx
+++ datasets/links.tsx
@@ -2,7 +2,7 @@
 export const instagram =
   "https://www.instagram.com/nu_mresconference?igsh=cTd1bmtwZWtiNjZ1&utm_source=qr";
 export const linkedin =
-  "www.linkedin.com/in/perspectives-patterns-and-pathways-32b873351";
+  "https://www.linkedin.com/in/perspectives-patterns-and-pathways-32b873351";
 
 export const ticketsLink =
   "https://www.eventbrite.co.uk/e/perspectives-patterns-and-pathways-tickets-1251596782669";
