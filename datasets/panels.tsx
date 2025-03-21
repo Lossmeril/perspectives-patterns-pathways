d7acc74c6a1fd6b144ed050253387e59dea95cd4
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2025-03-21 23:08:50 +0000

    fix: time schedule

--- datasets/panels.tsx
+++ datasets/panels.tsx
@@ -39,9 +39,9 @@ export const panels: Panel[] = [
     desc: "Our point of view is never a carbon copy of another person's. We are each individual and therefore we each bring a unique perspective. By bringing people together, we are able to broaden our own horizons and satiate our need to expand our knowledge.",
     speakers: [
       { speaker: speakers[7] },
-      { speaker: speakers[1] },
       { speaker: speakers[8] },
       { speaker: speakers[0] },
+      { speaker: speakers[1] },
     ],
     place: places[1],
     timeStart: "11:30 AM",
@@ -58,10 +58,10 @@ export const panels: Panel[] = [
     name: "Patterns",
     desc: "There is an intrinsic need to organise our understanding of culture, politics, history and society. Many before us have identified sequences and found relatability from a vast expanse of knowledge.",
     speakers: [
+      { speaker: speakers[9] },
       { speaker: speakers[6] },
       { speaker: speakers[10] },
       { speaker: speakers[3] },
-      { speaker: speakers[9] },
     ],
     place: places[1],
     timeStart: "1:30 PM",
@@ -71,9 +71,9 @@ export const panels: Panel[] = [
     name: "Pathways",
     desc: "We have come a long way and we are following in the footsteps of others. Interdisciplinarity is a natural byproduct of academic research, especially research that is not confined to strict disciplinary borders. Yet, there is one question we must always consider: where will the path take us next?",
     speakers: [
+      { speaker: speakers[5] },
       { speaker: speakers[4] },
       { speaker: speakers[2] },
-      { speaker: speakers[5] },
     ],
     place: places[1],
     timeStart: "2.53 PM",
