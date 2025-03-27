4bcd3620a8b0daee381a26d03c5346b6dfb42c04
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2025-03-27 16:02:13 +0000

    feat: trigger warning added

--- datasets/triggers.tsx
+++ datasets/triggers.tsx
@@ -10,4 +10,5 @@ export const triggerWarnings: TriggerWarning[] = [
   // { id: "violence", text: "Violence" },
   { id: "child-violence", text: "Violence towards children" },
   { id: "gender-based-violence", text: "Gender-based violence" },
+  { id: "15-plus", text: "Suitable for ages 15 and up" },
 ];
